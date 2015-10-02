b6ecca7b9662f4f8d3b4ba5a8f3cb9618c6521d3
V1TSK <vitaly@easycoding.org>
2015-10-02 22:33:11 +0500

    Исправлен год в копирайтах.

--- gchclient/Checker.cs
+++ gchclient/Checker.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Класс проверки приложения Garant Checker Offline.
  * 
- * Copyright 2012 - 2013 EasyCoding Team (ECTeam).
- * Copyright 2005 - 2013 EasyCoding Team.
+ * Copyright 2012 - 2015 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2015 EasyCoding Team.
  * 
  * Лицензия кода: модифицированная лицензия BSD.
  * Лицензия контента: Creative Commons 3.0 BY.
