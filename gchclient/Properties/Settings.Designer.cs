5569e62aed2366db70fca83800fbf4f24955886e
Vitaly Zaitsev <vitaly@easycoding.org>
2016-12-31 17:24:45 +0300

    SSL теперь включён по дефолту.

--- gchclient/Properties/Settings.Designer.cs
+++ gchclient/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace gchclient.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool UseSSL {
             get {
                 return ((bool)(this["UseSSL"]));
