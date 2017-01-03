3671d42612d8c277617a31845d40e06feb2e64f0
Vitaly Zaitsev <vitaly@easycoding.org>
2017-01-03 20:05:56 +0300

    Добавлена документация класса NativeFn. Все импортированные функции WinAPI теперь объявлены закрытыми.

--- gchcore/NativeFn.cs
+++ gchcore/NativeFn.cs
@@ -17,9 +17,15 @@ using System.Runtime.InteropServices;
 
 namespace gchcore
 {
+    /// <summary>
+    /// Класс импорта native-функций приложения.
+    /// </summary>
     public sealed class NativeFn
     {
-        public enum ShowWindowCommand
+        /// <summary>
+        /// Enum с кодами, используемыми native-функциями API.
+        /// </summary>
+        private enum ShowWindowCommand
         {
             SW_HIDE = 0,
             SW_SHOWNORMAL = 1,
@@ -36,13 +42,52 @@ namespace gchcore
             SW_MAX = 11
         }
 
-        [DllImport("user32")] public static extern IntPtr GetForegroundWindow();
-        [DllImport("user32")] public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr lpdwProcessId);
-        [DllImport("user32")] public static extern bool AttachThreadInput(IntPtr idAttach, IntPtr idAttachTo, bool fAttach);
-        [DllImport("user32")] public static extern bool SetForegroundWindow(IntPtr hWnd);
-        [DllImport("user32")] public static extern bool IsIconic(IntPtr hWnd);
-        [DllImport("user32")] public static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
+        /// <summary>
+        /// Возвращает handle окна, находящегося в фокусе.
+        /// </summary>
+        [DllImport("user32")] private static extern IntPtr GetForegroundWindow();
 
+        /// <summary>
+        /// Возвращает handle потока, создавшего окно с указанным handle.
+        /// </summary>
+        /// <param name="hWnd">Handle окна</param>
+        /// <param name="lpdwProcessId">Указатель на идентификатор процесса</param>
+        [DllImport("user32")] private static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr lpdwProcessId);
+
+        /// <summary>
+        /// Подключает поток ввода к другому потоку выполнения.
+        /// </summary>
+        /// <param name="idAttach">Handle потока, который нужно присоединить</param>
+        /// <param name="idAttachTo">Handle пока, к которому нужно присоединить</param>
+        /// <param name="fAttach">Булево присоединения: true - присоединить; false - отсоединить.</param>
+        /// <returns>Возвращает булево результата операции</returns>
+        [DllImport("user32")] private static extern bool AttachThreadInput(IntPtr idAttach, IntPtr idAttachTo, bool fAttach);
+
+        /// <summary>
+        /// Устанавливает фокус на указанное окно.
+        /// </summary>
+        /// <param name="hWnd">Handle окна</param>
+        /// <returns>Возвращает булево результата операции</returns>
+        [DllImport("user32")] private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// Проверяет свёрнуто ли окно.
+        /// </summary>
+        /// <param name="hWnd">Handle окна</param>
+        /// <returns>Возвращает результат проверки</returns>
+        [DllImport("user32")] private static extern bool IsIconic(IntPtr hWnd);
+
+        /// <summary>
+        /// Задаёт параметры указанного окна.
+        /// </summary>
+        /// <param name="hWnd">Handle окна</param>
+        /// <returns>Возвращает булево результата операции</returns>
+        [DllImport("user32")] private static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
+
+        /// <summary>
+        /// Работает с окном непосредственно: разворачивает / сворачивает.
+        /// </summary>
+        /// <param name="hWnd">Handle окна</param>
         public static void ActivateWindow(IntPtr hWnd)
         {
             IntPtr FGWndPntr = GetForegroundWindow();
