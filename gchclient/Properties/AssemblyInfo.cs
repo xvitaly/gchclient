6030e7ee45030aab94ff77342fef86a74222e48e
Vitaly Zaitsev <vitaly@easycoding.org>
2017-01-26 18:38:54 +0300

    Инкрементирован билд.

--- gchclient/Properties/AssemblyInfo.cs
+++ gchclient/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.759")]
-[assembly: AssemblyFileVersion("1.0.0.759")]
+[assembly: AssemblyVersion("1.0.0.761")]
+[assembly: AssemblyFileVersion("1.0.0.761")]
 [assembly: NeutralResourcesLanguageAttribute("ru")]
