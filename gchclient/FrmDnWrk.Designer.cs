3547c2ce09a498d98a8c0ef7cf409f4bde421be6
Vitaly Zaitsev <vitaly@easycoding.org>
2017-01-30 20:22:53 +0300

    Добавлен модуль загрузок.

---
+++ gchclient/FrmDnWrk.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace gchclient
+{
+    partial class FrmDnWrk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DN_PrgBr = new System.Windows.Forms.ProgressBar();
+            this.DN_WlcMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DN_PrgBr
+            // 
+            this.DN_PrgBr.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.DN_PrgBr.Location = new System.Drawing.Point(12, 35);
+            this.DN_PrgBr.Name = "DN_PrgBr";
+            this.DN_PrgBr.Size = new System.Drawing.Size(354, 29);
+            this.DN_PrgBr.TabIndex = 3;
+            // 
+            // DN_WlcMsg
+            // 
+            this.DN_WlcMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.DN_WlcMsg.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.DN_WlcMsg.Location = new System.Drawing.Point(12, 9);
+            this.DN_WlcMsg.Name = "DN_WlcMsg";
+            this.DN_WlcMsg.Size = new System.Drawing.Size(354, 19);
+            this.DN_WlcMsg.TabIndex = 2;
+            this.DN_WlcMsg.Text = "Downloading file... Please wait.";
+            this.DN_WlcMsg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmDnWrk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 78);
+            this.Controls.Add(this.DN_PrgBr);
+            this.Controls.Add(this.DN_WlcMsg);
+            this.Name = "FrmDnWrk";
+            this.Text = "Модуль загрузок";
+            this.Load += new System.EventHandler(this.FrmDnWrk_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar DN_PrgBr;
+        private System.Windows.Forms.Label DN_WlcMsg;
+    }
+}
---
+++ gchclient/FrmDnWrk.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace gchclient
+{
+    /// <summary>
+    /// Класс формы модуля загрузки файлов из Интернета.
+    /// </summary>
+    public partial class FrmDnWrk : Form
+    {
+        /// <summary>
+        /// Хранит статус выполнения процесса загрузки.
+        /// </summary>
+        private bool IsRunning = true;
+
+        /// <summary>
+        /// Хранит URL файла, который нужно загрузить.
+        /// </summary>
+        private string RemoteURI;
+
+        /// <summary>
+        /// Хранит путь к локальному файлу, куда нужно сохранить результат.
+        /// </summary>
+        private string LocalFile;
+
+        /// <summary>
+        /// Конструктор класса формы модуля загрузки файлов из Интернета.
+        /// </summary>
+        /// <param name="R">URL файла загрузки</param>
+        /// <param name="L">Путь сохранения файла</param>
+        public FrmDnWrk(string R, string L)
+        {
+            InitializeComponent();
+            RemoteURI = R;
+            LocalFile = L;
+        }
+
+        /// <summary>
+        /// Метод, срабатывающий при возникновении события "загрузка формы".
+        /// </summary>
+        private void FrmDnWrk_Load(object sender, EventArgs e)
+        {
+            // Начинаем процесс загрузки в отдельном потоке...
+            DownloaderStart(RemoteURI, LocalFile);
+        }
+
+        /// <summary>
+        /// Метод, информирующий основную форму о прогрессе загрузки файлов, который
+        /// выполняется в отдельном потоке.
+        /// </summary>
+        private void DownloaderProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Отрисовываем статус в прогресс-баре...
+            try { DN_PrgBr.Value = e.ProgressPercentage; } catch { /* Do nothing. */ }
+        }
+
+        /// <summary>
+        /// Метод, срабатывающий по окончании работы механизма загрузки в отдельном потоке.
+        /// </summary>
+        private void DownloaderCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            // Загрузка завершена. Проверим скачалось ли что-то. Если нет, удалим пустой файл...
+            try
+            {
+                if (File.Exists(LocalFile))
+                {
+                    FileInfo Fi = new FileInfo(LocalFile);
+                    if (Fi.Length == 0)
+                    {
+                        File.Delete(LocalFile);
+                    }
+                }
+            }
+            catch { /* Do nothing. */ }
+
+            // Закроем форму...
+            IsRunning = false;
+            Close();
+        }
+
+        /// <summary>
+        /// Метод, инициирущий процесс загрузки файла из Интернета.
+        /// </summary>
+        private void DownloaderStart(string URI, string FileName)
+        {
+            try
+            {
+                // Проверим существование файла и удалим...
+                if (File.Exists(FileName)) { File.Delete(FileName); }
+
+                // Начинаем асинхронную загрузку файла...
+                using (WebClient FileDownloader = new WebClient())
+                {
+                    FileDownloader.Headers.Add("User-Agent", Properties.Resources.AppUserAgent);
+                    FileDownloader.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloaderCompleted);
+                    FileDownloader.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloaderProgressChanged);
+                    FileDownloader.DownloadFileAsync(new Uri(URI), FileName);
+                }
+            }
+            catch { /* Do nothing. */ }
+        }
+
+        /// <summary>
+        /// Метод, срабатывающий при попытке закрытия формы.
+        /// </summary>
+        private void frmDnWrk_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = IsRunning;
+        }
+    }
+}
