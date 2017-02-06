7dd9076ead98098302bd627f61549d9b1284b255
Vitaly Zaitsev <vitaly@easycoding.org>
2017-02-06 19:35:19 +0300

    Добавлен новый значок в ресурс.

--- gchclient/Properties/Resources.Designer.cs
+++ gchclient/Properties/Resources.Designer.cs
@@ -404,6 +404,16 @@ namespace gchclient.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap clock {
+            get {
+                object obj = ResourceManager.GetObject("clock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Кастомное описание и дополнительная информация для этого пользователя отсутствуют..
         /// </summary>
         internal static string CustInfoNone {
