6ba2f91922142834304319c0d271b8ecebbbc70e
Vitaly Zaitsev <vitaly@easycoding.org>
2017-02-07 18:37:50 +0300

    Добавлен новый статический класс WxManager.

---
+++ gchclient/WxManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gchclient
+{
+    public static class WxManager
+    {
+        public static void ShowReportForm(string SteamID)
+        {
+            frmReportU FrmRep = new frmReportU(SteamID);
+            FrmRep.ShowDialog();
+        }
+    }
+}
