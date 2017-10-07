d1384ad32b276aebfa75e9a911c0a5fa1114ffcb
Vitaly Zaitsev <vitaly@easycoding.org>
2017-10-07 19:27:48 +0300

    Инкрементирована версия проекта.

--- gchclient/Properties/AssemblyInfo.cs
+++ gchclient/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.900")]
-[assembly: AssemblyFileVersion("2.0.0.900")]
+[assembly: AssemblyVersion("2.1.0.920")]
+[assembly: AssemblyFileVersion("2.1.0.920")]
 [assembly: NeutralResourcesLanguageAttribute("ru")]
